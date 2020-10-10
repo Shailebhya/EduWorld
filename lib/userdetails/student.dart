8ce68ea374dd510fa75d9f499d84c0bfff3b36c9
Shatakchi <shatakchisahay290@gmail.com>
2020-10-11 02:12:25 +0530

    Editing7

--- lib/userdetails/student.dart
+++ lib/userdetails/student.dart
@@ -17,111 +17,125 @@ class _getInfoState extends State<getInfo> {
   Card buildkey(String t,[String c])
   {
     return Card(
-      color: Colors.blue[900],
+      color: Colors.white,
       child: ListTile(
         dense:true,
-        title: Text("$t: ",  style: TextStyle(color: Colors.white70, fontSize: 20)),
-        subtitle: Text("$c",style: TextStyle(color: Colors.white70, fontSize: 18) ),
+        title: Text("$t: ",  style: TextStyle(color: Colors.black, fontSize: 16)),
+        subtitle: Text("$c",style: TextStyle(color: Colors.black, fontSize: 18) ),
       ),
     );
   }
   @override
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.orange[700],
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12.0, 4.0, 150.0, 4.0),
-              child: RaisedButton(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue[400], Colors.green[400]],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Scaffold(
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.transparent,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 4.0, 150.0, 4.0),
+                child: RaisedButton(
+                  color: Colors.blue[900],
+                  child: Text("Edit",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 18,
+                      )),
+                  onPressed: () =>Navigator.push(context, MaterialPageRoute(builder: (context)=> addScreenS())),
+                ),
+              ),
+              SizedBox(width: 180,),
+              RaisedButton(
                 color: Colors.blue[900],
-                child: Text("Edit",
+                child: Text("Logout",
                     style: TextStyle(
                       color: Colors.white70,
-                      fontSize: 20,
+                      fontSize: 18,
                     )),
-                onPressed: () =>Navigator.push(context, MaterialPageRoute(builder: (context)=> addScreenS())),
-              ),
-            ),
-            RaisedButton(
-              color: Colors.blue[900],
-              child: Text("Logout",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 20,
-                  )),
-              onPressed:logout,
-            )
-          ],
+                onPressed:logout,
+              )
+            ],
+          ),
         ),
-      ),
-      backgroundColor: Colors.orange[200],
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(currentUser.photoUrl)
-                    ),
-                      color: Colors.orange[300],
-                      borderRadius: BorderRadius.all(Radius.circular(35.0))),
-                  height: 170,
-                  width: 150,
-                ),
-                Expanded(
-                  child: Container(
-                    height: 150,
-                    child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(currentUser.displayName,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 25,
-                              )),
-                        )),
-                  ),
-                )
-              ],
-            ),
-            Expanded(
-              child: ListView(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  buildkey("About me",currentUser.bio),
-                  buildkey("Email ID",currentUser.email),
-                  buildkey("Age"),
-                  buildkey("Location"),
-                  buildkey("Gender"),
-                  buildkey("Grade"),
-                  buildkey("Preferred mode of tutoring"),
-                  buildkey("Need help in subjects"),
-                  buildkey("Timings"),
-                  buildkey("Ph number"),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(currentUser.photoUrl)
+                      ),
+                        gradient: LinearGradient(
+                          colors: [Colors.green[400], Colors.blue[400]],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(35.0))),
+                    height: 130,
+                    width: 140,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 150,
+                      child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(currentUser.displayName,
+                                style: TextStyle( fontFamily: "DancingScript",
+                                  color: Colors.black,
+                                  fontSize: 40,
+                                )),
+                          )),
+                    ),
+                  )
                 ],
               ),
-            )
+              Expanded(
+                child: ListView(
+                  children: [
+                    buildkey("About me",currentUser.bio),
+                    buildkey("Email ID",currentUser.email),
+                    buildkey("Age"),
+                    buildkey("Location"),
+                    buildkey("Gender"),
+                    buildkey("Grade"),
+                    buildkey("Preferred mode of tutoring"),
+                    buildkey("Need help in subjects"),
+                    buildkey("Timings"),
+                    buildkey("Ph number"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Center(
+              child: Text("User Details: ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                  ))),
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(icon: Icon(Icons.home, size: 35, color: Colors.blue[900],),
+              onPressed:()=> print("Put in MyHome Page here"),
+            ),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Center(
-            child: Text("User Details: ",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25,
-                ))),
-        backgroundColor: Colors.orange[300],
-        actions: [
-          IconButton(icon: Icon(Icons.home, size: 35, color: Colors.blue[900],),
-            onPressed:()=> print("Put in MyHome Page here"),
-          ),
-        ],
       ),
     );
   }
--- lib/userdetails/teacher.dart
+++ lib/userdetails/teacher.dart
@@ -16,112 +16,126 @@ class _getInfoTState extends State<getInfoT> {
   Card buildkey(String t,[String c])
   {
     return Card(
-      color: Colors.blue[900],
+      color: Colors.white,
       child: ListTile(
         dense:true,
-        title: Text("$t:",  style: TextStyle(color: Colors.white70, fontSize: 16)),
-      subtitle: Text("$c",style: TextStyle(color: Colors.white70, fontSize: 18) ),
+        title: Text("$t:",  style: TextStyle(color: Colors.black, fontSize: 16)),
+      subtitle: Text("$c",style: TextStyle(color: Colors.black, fontSize: 18) ),
       ),
     );
   }
   @override
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.deepOrange[700],
-        //color: Colors.orange[200],
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12.0, 2.0, 150.0, 4.0),
-              child: RaisedButton(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blue[400], Colors.green[400]],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Scaffold(
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.transparent,
+          //color: Colors.orange[200],
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 2.0, 12.0, 4.0),
+                child: RaisedButton(
+                  color: Colors.blue[900],
+                  child: Text("Edit",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 18,
+                      )),
+                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context)=>addScreen())),
+                ),
+              ),
+              SizedBox(width: 180,),
+              RaisedButton(
                 color: Colors.blue[900],
-                child: Text("Edit",
+                child: Text("Log Out",
                     style: TextStyle(
                       color: Colors.white70,
                       fontSize: 18,
                     )),
-                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context)=>addScreen())),
-              ),
-            ),
-            RaisedButton(
-              color: Colors.blue[900],
-              child: Text("Log Out",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 18,
-                  )),
-              onPressed: logout,
-            )
-          ],
+                onPressed: logout,
+              )
+            ],
+          ),
         ),
-      ),
-      backgroundColor: Colors.orange[200],
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(currentUser.photoUrl)
-                      ),
-                      color: Colors.orange[300],
-                      borderRadius: BorderRadius.all(Radius.circular(35.0))),
-                  height: 130,
-                  width: 140,
-                ),
-                Expanded(
-                  child: Container(
-                    height: 150,
-                    child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Text(currentUser.displayName,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                              )),
-                        )),
-                  ),
-                )
-              ],
-            ),
-            Expanded(
-              child: ListView(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  buildkey("About me", currentUser.bio),
-                  buildkey("Email ID", currentUser.email),
-                  buildkey("Education"),
-                  buildkey("Age"),
-                  buildkey("Location"),
-                  buildkey("Gender"),
-                  buildkey("Mode of tutoring"),
-                  buildkey("Price"),
-                  buildkey("Rating/Subject Knowledge"),
-                  buildkey("Timings"),
-                  buildkey("Ph number"),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(currentUser.photoUrl)
+                        ),
+                        gradient: LinearGradient(
+                          colors: [Colors.green[400], Colors.blue[400]],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(35.0))),
+                    height: 130,
+                    width: 140,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 150,
+                      child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(currentUser.displayName,
+                                style: TextStyle( fontFamily: "DancingScript",
+                                  color: Colors.black,
+                                  fontSize: 40,
+                                )),
+                          )),
+                    ),
+                  )
                 ],
               ),
-            )
+              Expanded(
+                child: ListView(
+                  children: [
+                    buildkey("About me", currentUser.bio),
+                    buildkey("Email ID", currentUser.email),
+                    buildkey("Education"),
+                    buildkey("Age"),
+                    buildkey("Location"),
+                    buildkey("Gender"),
+                    buildkey("Mode of tutoring"),
+                    buildkey("Price"),
+                    buildkey("Rating/Subject Knowledge"),
+                    buildkey("Timings"),
+                    buildkey("Ph number"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("User Profile ",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              )),
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(icon: Icon(Icons.home, size: 35, color: Colors.blue[900],),
+            onPressed:()=> print("Put in MyHome Page here"),
+            ),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text("User Profile ",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 22,
-            )),
-        backgroundColor: Colors.orange[300],
-        actions: [
-          IconButton(icon: Icon(Icons.home, size: 35, color: Colors.blue[900],),
-          onPressed:()=> print("Put in MyHome Page here"),
-          ),
-        ],
       ),
     );
   }
